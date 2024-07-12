a98592819447436cfce88746d08fe832fa488642
CharityNambi05 <charity.nambi@icloud.com>
2024-07-12 21:46:32 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+};
+
+// Testing the function
+console.log(mondayWork()); // This Monday, I will go to the office.
+console.log(mondayWork("work from home")); // This Monday, I will work from home.
+
+function wrapAdjective(wrapper) {
+    return function(adjective) {
+        return `You are ${wrapper}${adjective}${wrapper}!`;
+    };
+}
+
+// Testing the function
+const wrapWithStars = wrapAdjective('*');
+console.log(wrapWithStars('amazing')); // You are *amazing*!
+
+const wrapWithPipes = wrapAdjective('||');
+console.log(wrapWithPipes('a genius')); // You are ||a genius||!
